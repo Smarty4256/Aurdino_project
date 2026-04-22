dea499b99182d340420d8e494c8ca1b36755906a
sheshu <sheshugandhasiri@gmail.com>
2026-04-22 22:47:32 +0530

    updated BE and FE

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import streamlit as st
 import requests
 
-# 🔥 USE YOUR RENDER BACKEND URL
+st.set_page_config(
+    page_title="Sheshu IoT Control 🚀",
+    page_icon="💡"
+)
+
 API = "https://aurdino-project.onrender.com"
 
-st.title("💡 LED Control")
+st.title("💡 Sheshu Smart Home Control")
+st.caption("Control your devices from anywhere 🌍")
 
-if st.button("ON"):
+if st.button("Turn ON"):
     res = requests.get(f"{API}/on")
     st.success(res.json()["status"])
 
-if st.button("OFF"):
+if st.button("Turn OFF"):
     res = requests.get(f"{API}/off")
     st.error(res.json()["status"])
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from fastapi import FastAPI
-# import serial
-# import time
 
 app = FastAPI()
 
-# arduino = serial.Serial('COM7', 9600)  # change COM
-# time.sleep(1)
+latest_command = "OFF"
 
 @app.get("/")
 def home():
     return {"msg": "API running"}
+
 @app.get("/on")
 def led_on():
+    global latest_command
+    latest_command = "ON"
     return {"status": "ON"}
+
 @app.get("/off")
 def led_off():
+    global latest_command
+    latest_command = "OFF"
     return {"status": "OFF"}
 
+@app.get("/status")
+def get_status():
+    return {"command": latest_command}
